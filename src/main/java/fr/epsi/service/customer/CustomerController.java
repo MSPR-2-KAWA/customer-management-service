a092130ab433523ab3273abb521e5bfbb655a3ca
Lheniart <146065771+Lheniart@users.noreply.github.com>
2025-05-05 15:46:35 +0200

    feat(endpoit): add first endpoint (#1)

--- src/main/java/fr/epsi/service/customer/CustomerController.java
+++ src/main/java/fr/epsi/service/customer/CustomerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     @GetMapping("/customer")
-    public String HelloWorld() {
+    public String helloWorld() {
         return "Hello world";
     }
 }
